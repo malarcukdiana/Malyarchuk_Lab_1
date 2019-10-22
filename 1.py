07608c232a0cafe99a93982f03233ae487b59fd1
malarcukdiana <56016905+malarcukdiana@users.noreply.github.com>
2019-10-22 10:49:01 +0300

    Лабораторна робота 1

---
+++ 1.py
@@ -0,0 +1,5 @@
+S = int(input("Enter S = "))
+a = int(input("Enter a = "))
+d = a ** 2 + 8 * S
+h = ( -a + d ** 0.5 ) / 2
+print("h = ", h)
---
+++ 2.py
@@ -0,0 +1,5 @@
+R1 = float (input("Enter R1 "))
+R2 = float (input("Enter R2 "))
+T1 = float (input("Enter T1 "))
+T2 = ((T1 ** 2 * R2 ** 3)/(R1 ** 3))** 0.5
+print (T2)
